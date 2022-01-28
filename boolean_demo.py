252dedea33fda69f17938dfd5d591201a6104684
j3tj3rk <jclea1130@gmail.com>
2022-01-28 11:39:49 -0500

    Create boolean_demo.py

---
+++ boolean_demo.py
@@ -0,0 +1,57 @@
+valid = True
+not_valid = False
+
+print(valid)
+print(not_valid)
+
+print(valid == True)
+print(not_valid == True)
+
+print(valid != True)
+print(not_valid != True)
+
+print( not valid)
+print(not not_valid)
+
+print((10 < 9) == True)
+print((10 == 10) == True)
+print((10 != 10) == True)
+print((10 >= 10) == True)
+print((10 <= 10) == True)
+print((10 > 9) == True)
+
+print((10 < 9))
+print((10 == 10))
+print((10 != 10))
+print((10 >= 10))
+print((10 <= 10))
+print((10 > 9))
+
+print("---------------")
+
+print(10 > 5 and 10 < 5)
+print(10 > 5 or 10 < 5)
+
+print(bool(0))
+print(bool(1))
+
+print(bool(0) == False)
+print(bool(1) == True)
+
+print(10 + 10)
+print(10 - 10)
+print(10 / 10)
+print(10 // 10)
+
+print(10 / 3)
+print(10 //3)
+print(10 % 3)
+
+print(10 * 10)
+print(10 ** 10)
+print(10 %10)
+
+x = 10
+print(x)
+x = x + 1
+print(x)
